d8d3c299b919abc5e145a289c08b8f45bea12071
Kenneth Scianna <kscianna@skylarkwireless.com>
2020-05-27 12:52:47 -0500

    Added a git hash to pyfaros install.

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from __future__ import absolute_import
 import setuptools
+import git
+
+sha = git.Repo(search_parent_directories=True).head.object.hexsha[:8]
+
 # fixme: this excludes SoapySDR
 with open('requirements.txt') as fp:
   install_requires = fp.read()
 
 setuptools.setup(
     name="pyfaros",
-    version="0.0.4",
+    version="0.0.4-{}".format(sha),
     author="Skylark Wireless",
     description="discover/update skylark wireless devices",
     url="https://gitlab.com/skylark-wireless/software/sklk-utils/",
